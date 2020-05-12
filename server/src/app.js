c4618a21ebb48ee8e6e7342de47f926f1fc19efe
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-05-12 18:13:04 +0300

    serve react app

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const { join } = require('path');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', '..', 'client', 'build')));
+  app.all('*', (req, res) =>
+    res.sendFile(join(__dirname, '..', '..', 'client', 'build', 'index.html'))
+  );
+}
 
 module.exports = app;
